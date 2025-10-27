17d22d4336f2aa8a1002cd1da413c7016a5b594a
Maid <muhrafly1@gmail.com>
2025-10-27 15:45:29 +0800

    Create maid-bypass.user.js

---
+++ maid-bypass.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Maid Bypass - Ultimate Edition
+// @namespace    http://tampermonkey.net/
+// @version      2.0.0
+// @description  Auto bypass for Volcano & Work.ink - Indonesian & English support
+// @author       Maid Dev
+// @match        https://key.volcano.wtf/*
+// @match        https://work.ink/*
+// @run-at       document-start
+// @grant        unsafeWindow
+// @icon         https://i.postimg.cc/y6Gx5NBD/volcano.png
+// @downloadURL  https://raw.githubusercontent.com/MaidDev/maid-bypass/main/maid-bypass.user.js
+// @updateURL    https://raw.githubusercontent.com/MaidDev/maid-bypass/main/maid-bypass.user.js
+// @require      https://raw.githubusercontent.com/MaidDev/maid-bypass/refs/heads/main/maid-bypass.js
+// ==/UserScript==
